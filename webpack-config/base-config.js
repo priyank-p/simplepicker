153ea47128c59b522c8e5f295bc926d66c9b7b14
Priyank Patel <priyankp390@gmail.com>
2018-08-30 18:23:17 -0400

    css: use webpack to bundle and minify css.

--- webpack-config/base-config.js
+++ webpack-config/base-config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 const ROOT_DIR = path.resolve(__dirname, '..');
 const config = {
@@ -13,18 +14,35 @@ const config = {
   devtool: 'source-map',
   plugins: [
     new webpack.HashedModuleIdsPlugin(),
+    new MiniCSSExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
   ]
 };
 
 config.module = {};
 config.module.rules = [
-  { 
+  {
     test: /\.js$/,
     exclude: /node_modules/,
     use: {
       loader: 'babel-loader',
       options: { presets: ['@babel/env'] }
     }
+  },
+  {
+    test: /\.css$/,
+    use: [
+      MiniCSSExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          minimize: true
+        }
+      },
+    ]
   }
 ];
 
--- webpack-config/min.js
+++ webpack-config/min.js
@@ -1,6 +1,7 @@
 const config = require('./base-config');
 
 config.entry = {
+  'simplepicker': './lib/simplepicker.css',
   'simplepicker.min': './lib/index.js'
 };
 
