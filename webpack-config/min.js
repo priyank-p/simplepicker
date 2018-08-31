f0bd66b61d3fc5cd0dfe0334735fefd7a59251c7
Priyank Patel <priyankp390@gmail.com>
2018-08-31 09:36:56 -0400

    webpack: remove building for node.

--- webpack-config/min.js
+++
@@ -1,9 +0,0 @@
-const basConfig = require('./base-config');
-
-const config = Object.assign(basConfig, {});
-config.entry = {
-  'simplepicker': './lib/simplepicker.css',
-  'simplepicker.min': './lib/index.js'
-};
-
-module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,53 @@
-const minConfig = require('./webpack-config/min');
-const nodeConfig = require('./webpack-config/node');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = [ minConfig, nodeConfig ];
+const ROOT_DIR = __dirname;
+const config = {
+  entry: {
+    'simplepicker': './lib/simplepicker.css',
+    'simplepicker.min': './lib/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(ROOT_DIR, 'dist'),
+  },
+  context: ROOT_DIR,
+  target: 'web',
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new MiniCSSExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
+
+config.module = {};
+config.module.rules = [
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: { presets: ['@babel/env'] }
+    }
+  },
+  {
+    test: /\.css$/,
+    use: [
+      MiniCSSExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          minimize: true
+        }
+      },
+    ]
+  }
+];
+
+module.exports = config;
